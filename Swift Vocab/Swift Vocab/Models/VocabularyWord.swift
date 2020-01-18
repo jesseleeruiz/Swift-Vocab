b291ba8c2626c41d17a8e5bfe69198146669d2ff
jesseleeruiz <jesseleeruiz@gmail.com>
2020-01-17 14:51:01 -1000

    Added Model

---
+++ Swift Vocab/Swift Vocab/Models/VocabularyWord.swift
@@ -0,0 +1,14 @@
+//
+//  VocabularyWord.swift
+//  Swift Vocab
+//
+//  Created by Jesse Ruiz on 1/17/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct VocabularyWord {
+    let word: String
+    let definition: String
+}
---
+++ Swift Vocab/Swift Vocab/View Controllers/DefinitionViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DefinitionViewController.swift
+//  Swift Vocab
+//
+//  Created by Jesse Ruiz on 1/17/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class DefinitionViewController: UIViewController {
+    
+    // MARK: Outlets
+    @IBOutlet weak var wordLabel: UILabel!
+    @IBOutlet weak var textView: UITextView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
